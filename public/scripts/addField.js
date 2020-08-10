e7b5a5324c3581cda2564ac155b8a6d6a2ccce01
Emerson Lima <emersondelimasilva@hotmail.com>
2020-08-10 11:45:32 -0300

    Versão 1.0.1
    Botão de remover horário adicionado

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,3 +1,4 @@
+contador = 0
 //Procurar o botão
 document.querySelector("#add-time")
 //Quando clicar no botão
@@ -10,4 +11,23 @@ function cloneField() {
         field.value = ""
     })
     document.querySelector('#schedule-items').appendChild(newfieldContainer)
+    contador += 1
+    console.log("Campo de novo horário adicionado com sucesso!")
 }
+
+//remover campo de novo horário
+document.querySelector("#del-time")
+//quando clicar no botão
+.addEventListener('click', removeField)
+
+//executar uma ação
+function removeField() {
+    const removefieldContainer = document.querySelector('.schedule-item')
+    const fields = removefieldContainer.querySelectorAll('input')
+    if (contador > 0) {
+        document.querySelector('#schedule-items').removeChild(removefieldContainer)
+        contador -= 1
+        console.log("Campo de novo horário removido com sucesso!")
+    }
+}
+
